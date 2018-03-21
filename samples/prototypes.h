4efed20a5e3dc8802f7d6861ba76e0ce7dc27712
vcheruiyot <vcheruiy@stanford.edu>
2018-03-20 21:18:17 -0700

    files for testing code

---
+++ samples/prototypes.h
@@ -0,0 +1,14 @@
+/* File: prototypes.h
+ * ------------------
+ * Defines the required interface for the functions of CS107 assign3.
+ * Refer to assignment writeup for more information.
+ */
+
+#ifndef _prototypes_h
+#define _prototypes_h
+
+#include <stdio.h>
+
+char *read_line(FILE *fp);
+
+#endif
